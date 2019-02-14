b73296983c502e7d894a97f10f85cfcaa4596cdf
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2019-02-14 23:50:04 +0100

    Add some more code to test coveralls

--- minima.py
+++ minima.py
@@ -8,6 +8,17 @@ def minima(arr):
 
     return ans
 
+def maxima(arr):
+    ans = []
+    i = 0
+
+    for i in range(len(arr) - 1):
+        if arr[i-1] > arr[i] and arr[i] < arr[i+1]:
+            ans.append(i)
+
+    return ans
+
+
 class Fake:
     def __init__(self):
         pass
